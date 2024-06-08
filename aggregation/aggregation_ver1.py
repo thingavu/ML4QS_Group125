64dc9aa9c95ed46403939029cc228384e7b0e824
durunazhan <durunaz2012@gmail.com>
2024-06-08 15:14:08 +0200

    merged all data (except autocorrelation) on 0.1 s

---
+++ aggregation/aggregation_ver1.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import glob
+import os
+from tqdm import tqdm
+
+import warnings
+warnings.filterwarnings("ignore")
+
+def read_data(path, split="/"):
+    data = pd.read_csv(path, sep=',')
+    parts = path.split(split)
+    trial = parts[-1].replace(".csv", "").split("_")
+
+    data["language"] = trial[0]
+    data["tone"] = trial[1]
+    data["participant"] = parts[-2]
+    data["script"] = trial[2]
+        
+    supportive_columns = [c for c in data.columns if c not in ["PitchSensor", "DecibelSource"]]
+    pitch_data = data[supportive_columns + ["PitchSensor"]]
+    pitch_data["sensor"] = "pitch"
+    pitch_data = pitch_data.rename(columns={"PitchSensor": "pitch", "relative_time": "time"})
+
+    intensity_data = data[supportive_columns + ["DecibelSource"]]
+    intensity_data["sensor"] = "sound_intensity"
+    intensity_data = intensity_data.rename(columns={"DecibelSource": "sound_intensity", "relative_time": "time"})
+    
+    return pitch_data, intensity_data
+
+def aggregation(data, measurement, delta=0.1):
+    data['time_in_sec'] = data['time'] / 1000
+    data[f'time_{delta}'] = (data['time_in_sec'] // delta).astype(int) * delta
+
+    def calculate_missing_percentage(group):
+        total_count = len(group)
+        missing_count = group[measurement].isna().sum()
+        missing_percentage = (missing_count / total_count) * 100
+        sensor = group['sensor'].iloc[0]
+
+        return pd.Series({
+            f'{sensor}_max': group[measurement].max(),
+            f'{sensor}_min': group[measurement].min(),
+            f'{sensor}_mean': group[measurement].mean(),
+            f'{sensor}_median': group[measurement].median(),
+            f'{sensor}_std': group[measurement].std(),
+            f'{sensor}_missing': missing_percentage
+        })
+
+    aggregated_data = data.groupby([f'time_{delta}', 'language', 'tone', 'participant', 'script', 'sensor']).apply(calculate_missing_percentage).reset_index()
+    return aggregated_data
+
+def main():
+    os.makedirs("./data_aggregated", exist_ok=True)
+
+    for delta in tqdm([0.1, 0.5, 1, 2, 5]):
+        pitch_merged = pd.DataFrame()
+        intensity_merged = pd.DataFrame()        
+        for path in glob.glob("data/intensity_pitch/*/*.csv"):
+            if "nga" in path:
+                continue
+            if "duru" in path:
+                continue
+            pitch_data, intensity = read_data(path)
+            pitch_aggregated = aggregation(pitch_data, "pitch", delta=delta)
+            intensity_aggregated = aggregation(intensity, "sound_intensity", delta=delta)
+
+            pitch_merged = pd.concat([pitch_merged, pitch_aggregated])
+            intensity_merged = pd.concat([intensity_merged, intensity_aggregated])
+        pitch_merged.to_csv(f"data_aggregated/pitch_time_{delta}.csv", index=False)
+        intensity_merged.to_csv(f"data_aggregated/intensity_time_{delta}.csv", index=False)
+
+
+if __name__ == '__main__':
+    main()
---
+++ lineplots/line_plots.py
@@ -0,0 +1,99 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def line_plots_combined(data, measure='pitch_mean', min_time=None, max_time=None, ):
+    sensor = data['sensor'].iloc[0]
+    languages = data['language'].unique()
+    tones = data['tone'].unique()
+
+    time_col = [c for c in data.columns if 'time' in c][0]
+    delta_t = float(time_col.split('_')[-1])
+
+    if min_time is None and max_time is None:
+        min_time = data[time_col].min()
+        max_time = data[time_col].max()
+    
+    data = data[(data[time_col] > min_time) & (data[time_col] < max_time)]
+
+    fig, axs = plt.subplots(4, 1, figsize=(18, 6), sharex=True, sharey=True)
+    axs = axs.flatten()
+    idx = 0
+
+    for language in languages:
+        for tone in tones:
+            ax = axs[idx]
+            filtered_data = data[(data['language'] == language) & (data['tone'] == tone)]
+            participants = filtered_data['participant'].unique()
+
+            for participant in participants:
+                participant_data = filtered_data[filtered_data['participant'] == participant]
+                scripts = participant_data['script'].unique()
+
+                for script in scripts:
+                    script_data = participant_data[participant_data['script'] == script]
+                    ax.plot(script_data[time_col], script_data[measure], label=f'{participant}-{script}')
+
+            ax.set_xlim(min_time, max_time)
+            if language == "en":
+                language_label = "Non-mother-tongue"
+            else:
+                language_label = "Mother-tongue"
+            
+            tone_label = tone
+            if tone == "bus":
+                tone_label = "business"
+            ax.set_ylabel(f'{sensor.capitalize()}', fontsize=14)
+            ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=8, fontsize=10)
+            ax.grid(True)
+            ax.set_title(f'{language_label} - {tone_label.capitalize()}', fontsize=14)
+            idx += 1
+
+    plt.xlabel(f'Time (Δt={delta_t}s)', fontsize=14)
+    plt.tight_layout()
+    file_name = f'./lineplots/{sensor}_delta{delta_t}.png'
+    plt.savefig(file_name)
+    plt.close()
+
+# def line_plots(data, measure='intensity_mean'):
+#     sensor = data['sensor'].iloc[0]
+#     languages = data['language'].unique()
+#     tones = data['tone'].unique()
+
+#     time_col = [c for c in data.columns if 'time' in c][0]
+#     delta_t = float(time_col.split('_')[-1])
+    
+#     def plot_and_save_intensity_float(data, language, tone):
+#         plt.figure(figsize=(18, 3))
+#         filtered_data = data[(data['language'] == language) & (data['tone'] == tone)]
+#         participants = filtered_data['participant'].unique()
+
+#         min_time = data[time_col].min()
+#         max_time = data[time_col].max()
+#         for participant in participants:
+#             participant_data = filtered_data[filtered_data['participant'] == participant]
+#             scripts = participant_data['script'].unique()
+            
+#             for script in scripts:
+#                 script_data = participant_data[participant_data['script'] == script]
+#                 plt.plot(script_data[time_col], script_data[measure], label=f'{participant}-{script}')
+        
+#         plt.xlim(min_time, max_time)
+#         # plt.xlabel(f'Time (Δt={delta_t})', fontsize=14)
+#         plt.ylabel(f'{sensor.capitalize()}', fontsize=14)
+#         # plt.title(f'{sensor.capitalize()} over Time (Δt={delta_t}) for Language: {language}, Tone: {tone}')
+#         plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=8, fontsize=12)
+#         plt.grid(True)
+        
+#         file_name = f'./lineplots/{sensor}_delta{delta_t}_{language}_{tone}.png'
+#         plt.savefig(file_name)
+#         plt.close()
+
+#     # Save plots for each combination of language and tone with float time values
+#     for language in languages:
+#         for tone in tones:
+#             plot_and_save_intensity_float(data, language, tone)
+
+# Call the function with the aggregated data
+data = pd.read_csv('data_aggregated/pitch_time_0.5.csv')
+line_plots_combined(data, measure='pitch_mean', min_time=100, max_time=120)
