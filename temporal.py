a997e27c1355c26e9a071b5c3072dab42ce9c19f
BCJACK0125 <orz.magic.warrior@gmail.com>
2024-06-12 11:55:08 +0200

    update temporal.py

--- temporal.py
+++ temporal.py
@@ -5,13 +5,15 @@ import pandas as pd
 data = pd.read_csv('./data_w_features/data_w_all_features.csv')
 
 # Target columns
-target_columns = ['amplitude_mean', 'pitch_mean', 'sound_intensity_mean']
+columns = data.columns - ['time_1','language','tone','participant','script']
+target_columns = columns - []
 
 # Create an instance of the TemporalAbstraction class
 temporal_abstraction = NumericalAbstraction()
 
 # Abstract the numerical data
-data = temporal_abstraction.abstract_numerical(data, target_columns, 5, 'slope')
+for abstration_function in ['mean', 'max', 'min', 'median', 'std', 'slope']:
+    data = temporal_abstraction.abstract_numerical(data, target_columns, 5, abstration_function)
 
 # Save the data
 data.to_csv('data_temporal_abstraction/data_w_all_features_and_temporal_abstraction.csv', index=False)
