cd7e02923d68ea6225d2cbc068ca43c51dd0dc08
thingavu <65972236+thingavu@users.noreply.github.com>
2024-06-13 14:02:20 +0200

    fix features

--- Chapter4/fourier_and_autocorrelation.py
+++ Chapter4/fourier_and_autocorrelation.py
@@ -22,23 +22,32 @@ def rolling_autocorrelation(series, window, max_lag):
 
 
 data = pd.read_csv('./data_preprocessed/preprocessed_merged_0.5.csv', index_col=0)
-print(data.head())
 
-# Apply Fourier transformation to the data
-ft = FourierTransformation()
-transformed_data = ft.abstract_frequency(data, columns=['sound_intensity_mean_kalman', 'amplitude_mean_kalman',\
-                                                         'pitch_mean_kalman'], window_size=10, sampling_rate=1)
+trials = data[['language', 'tone', 'participant', 'script']].drop_duplicates()
+trials = list(trials.itertuples(index=False, name=None))
+print("Number of trials: ", len(trials))
 
-# Get autocorrelation values for amplitude and sound intensity
-window_size = 10
-max_lag = 5
+result = pd.DataFrame()
+for lang, tone, part, script in trials:
+    data_subset = data[(data['language'] == lang) & (data['tone'] == tone) & (data['participant'] == part) & (data['script'] == script)]
 
-amplitude_autocorr = rolling_autocorrelation(transformed_data['amplitude_mean_kalman'], window_size, max_lag)
-sound_intensity_autocorr = rolling_autocorrelation(transformed_data['sound_intensity_mean_kalman'], window_size, max_lag)
+    # Apply Fourier transformation to the data
+    ft = FourierTransformation()
+    transformed_data = ft.abstract_frequency(data_subset, columns=['sound_intensity_mean_kalman', 'amplitude_mean_kalman',\
+                                                            'pitch_mean_kalman'], window_size=10, sampling_rate=1)
 
-# Add rolling autocorrelation values to the dataframe
-for lag in range(1, max_lag + 1):
-    transformed_data[f'amplitude_autocorr_lag_{lag}'] = amplitude_autocorr[f'rolling_autocorr_lag_{lag}']
-    transformed_data[f'sound_intensity_autocorr_lag_{lag}'] = sound_intensity_autocorr[f'rolling_autocorr_lag_{lag}']
+    # Get autocorrelation values for amplitude and sound intensity
+    window_size = 10
+    max_lag = 5
 
-transformed_data.to_csv('./data_w_features/data_w_all_features_0.5.csv', index=False)
+    amplitude_autocorr = rolling_autocorrelation(transformed_data['amplitude_mean_kalman'], window_size, max_lag)
+    sound_intensity_autocorr = rolling_autocorrelation(transformed_data['sound_intensity_mean_kalman'], window_size, max_lag)
+
+    # Add rolling autocorrelation values to the dataframe
+    for lag in range(1, max_lag + 1):
+        transformed_data[f'amplitude_autocorr_lag_{lag}'] = amplitude_autocorr[f'rolling_autocorr_lag_{lag}']
+        transformed_data[f'sound_intensity_autocorr_lag_{lag}'] = sound_intensity_autocorr[f'rolling_autocorr_lag_{lag}']
+    
+    result = pd.concat([result, transformed_data])
+
+result.to_csv('./data_w_features_ver2/data_w_fourier_and_autocorr_0.5.csv', index=False)
